8ef182abcf9f9105cef3d2b747650c9e5260dfa8
toni <easyrobotics2@gmail.com>
2023-08-12 12:35:23 +0200

    send open to esp

--- docker_opi_servers/web_server/index.js
+++ docker_opi_servers/web_server/index.js
@@ -1,5 +1,5 @@
-const http = require('./app')
+const http = require("./app");
 
 http.listen(3000, () => {
-    console.log('Servidor en el puerto 3000');
-})
+  console.log("Servidor en el puerto 3000");
+});

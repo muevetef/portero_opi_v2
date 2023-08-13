d672e49d888368ae01f2b2ce7b8f44af399ab324
toni <easyrobotics2@gmail.com>
2023-08-13 10:59:30 +0200

    Adjust Frameraate

--- opi_clients/camara_csi/config.h
+++ opi_clients/camara_csi/config.h
@@ -1,5 +1,5 @@
 #define FRAME_HEIGHT 480
 #define FRAME_WIDTH 640
-#define FRAME_INTERVAL (1000/10)
-#define PACK_SIZE 4096 //udp pack size; note that OSX limits < 8100 bytes
+#define FRAME_INTERVAL (0.6)
+#define PACK_SIZE 4096 // udp pack size; note that OSX limits < 8100 bytes
 #define ENCODE_QUALITY 80

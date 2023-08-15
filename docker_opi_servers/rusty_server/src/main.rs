778f591394b28d8dbb4168deca84d584d864e544
Stay1444 <quiquelluc@gmail.com>
2023-08-15 16:09:50 +0200

    format

--- docker_opi_servers/rusty_server/src/main.rs
+++ docker_opi_servers/rusty_server/src/main.rs
@@ -1,10 +1,12 @@
-use tracing::{info, error, Level};
+use tracing::{error, info, Level};
 
-mod web_server;
-mod frame_receiver;
-mod qr_scanner;
 mod esp_comm;
-mod utils;
+mod frame_receiver;
+mod models;
+mod qr_scanner;
+mod services;
+pub mod utils;
+mod web_server;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -18,19 +20,23 @@ async fn main() -> anyhow::Result<()> {
     let (esp_msg_tx, esp_msg_rx) = tokio::sync::mpsc::channel(64);
 
     let tasks = vec![
-        tokio::spawn(web_server::run(frame_tx.clone(), qr_tx.clone(), esp_msg_tx.clone())),
+        tokio::spawn(web_server::run(
+            frame_tx.clone(),
+            qr_tx.clone(),
+            esp_msg_tx.clone(),
+        )),
         tokio::spawn(qr_scanner::run(frame_tx.subscribe(), qr_tx.clone())),
         tokio::spawn(frame_receiver::run(frame_tx.clone())),
-        tokio::spawn(esp_comm::run(esp_msg_rx))
+        tokio::spawn(esp_comm::run(esp_msg_rx)),
     ];
 
     match futures::future::try_join_all(tasks).await {
         Ok(_) => {
             info!("All tasks executed successfully, exiting.");
-        },
+        }
         Err(err) => {
             error!("Unexpected error executing task(s): {err}");
-        },
+        }
     }
 
     Ok(())
